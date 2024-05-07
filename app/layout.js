d5d5f4c56aa3fb5773c0ef4e1cdaf29393e65b1b
Pegu-Mrigen <mrig8520@gmail.com>
2024-05-07 15:40:24 +0530

    07052024

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "ADMISSION-FORM_GIHS",
+  title: "ADMISSION-GIHS",
   description: "Gohpur Institute of Health & Science was established in the 2023 with a mission to provide affordable quality paramedical education to the youth of Assam, Which is situated in Gohpur, Biswanath, Assam. GIHS is affiliated to NCETIR (National Council of Educational Training Institute And Research).",
 };
 
