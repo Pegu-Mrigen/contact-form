06f0e2ec5799b691a6d83ebea2b870050c8e1996
Pegu-Mrigen <mrig8520@gmail.com>
2024-05-13 14:58:04 +0530

    13052024

--- components/RazorpayComponets/ProductList.jsx
+++ components/RazorpayComponets/ProductList.jsx
@@ -2,7 +2,7 @@
 import React, { Suspense } from "react";
 import Product from "./Product";
 import { useRouter } from "next/navigation";
-//import Loading from "../../components/Loading";
+import Loading from "../../components/Loading";
 
 const ProductList = () => {
   const router = useRouter();
@@ -73,8 +73,7 @@ const ProductList = () => {
 
   return (
     <>
-      {/* <Suspense fallback={<Loading />}> */}
-      <Suspense >
+      <Suspense fallback={<Loading />}>
         <Product makePayment={makePayment} />
       </Suspense>
     </>
