939b7f879792c096330a2d0523a5d0a31ce7ae9c
Pegu-Mrigen <mrig8520@gmail.com>
2024-05-13 14:50:25 +0530

    13052024

--- components/RazorpayComponets/ProductList.jsx
+++ components/RazorpayComponets/ProductList.jsx
@@ -2,7 +2,7 @@
 import React, { Suspense } from "react";
 import Product from "./Product";
 import { useRouter } from "next/navigation";
-import Loading from "./../Loading";
+import Loading from "../../components/Loading";
 
 const ProductList = () => {
   const router = useRouter();
